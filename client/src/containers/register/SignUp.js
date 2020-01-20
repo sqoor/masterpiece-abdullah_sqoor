08486ec31d03995f9bcb1c053f2bb4b4c395396e
sqoor <abdullahalsqoor2@gmail.com>
2020-01-20 10:39:42 +0200

    sing up build validation

--- client/src/containers/register/SignUp.js
+++ client/src/containers/register/SignUp.js
@@ -1,79 +1,132 @@
 import React, { Component } from "react";
 
+import Axios from "axios";
+import SimpleReactValidator from "simple-react-validator";
+import { ToastContainer, toast, Zoom } from "react-toastify";
+
 class SignUp extends Component {
   state = {
     name: "",
     email: "",
     password: "",
-    password_confirmation: ""
+    passwordConfirmation: ""
   };
 
-  changeHandler = e => {
-    this.setState({
-      [e.target.name]: e.target.value
+  constructor(props) {
+    super(props);
+    this.validator = new SimpleReactValidator({
+      element: message => (
+        <div className="alert alert-danger mt-2">{message}</div>
+      ),
+      validators: {
+        match: {
+          message: "Do not match the password.",
+          rule: val => val === this.state.password
+        }
+      }
     });
+  }
+
+  changeHandler = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  requestApi = credentials => {
+    Axios.post("/users/signup", { credentials })
+      .then(res => {
+        // do somesthing go to next page if has the token or respond to use unauthorized
+        console.log("RESPONSE", res);
+      })
+      .catch(error => {
+        console.log("ERROR", error);
+        toast.error("Something went wrong");
+      });
   };
 
   submitHandler = e => {
     e.preventDefault();
 
+    const { requestApi, state } = this;
+
+    if (this.validator.allValid()) {
+      requestApi(state);
+      console.log("Logining in...", state);
+    } else {
+      this.validator.showMessages();
+      this.forceUpdate();
+      toast.error("Please fill the empty input fields");
+    }
+
     console.log("Signing up...", this.state);
   };
 
   render() {
-    const { changeHandler, submitHandler } = this;
-    const { name, email, password, confirm } = this.state;
+    const { validator, changeHandler, submitHandler } = this;
+    const { name, email, password, passwordConfirmation } = this.state;
 
     return (
       <div className="container">
         <h1 className="text-center">Sign up Page</h1>
-          <form onSubmit={submitHandler}>
-            <div className="form-group">  
-              <label htmlFor="name">Name</label>
-              <input
-                id="name"
-                className="form-control"
-                type="text"
-                name="name"
-                value={name}
-                onChange={changeHandler}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                id="email"
-                className="form-control"
-                type="email"
-                name="email"
-                value={email}
-                onChange={changeHandler}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input
-                id="password"
-                className="form-control"
-                type="password"
-                name="password"
-                value={password}
-                onChange={changeHandler}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password_confirmation">Confirm Password</label>
-              <input
-                id="password_confirmation"
-                className="form-control"
-                type="password"
-                name="password_confirmation"
-                value={confirm}
-                onChange={changeHandler}
-              />
-            </div>
-            <input className="btn btn-primary" type="submit" value="Sign Up" />
-          </form>
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              className="form-control"
+              type="text"
+              name="name"
+              value={name}
+              onChange={changeHandler}
+            />
+            {validator.message("name", name, "required|alpha")}
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              className="form-control"
+              type="email"
+              name="email"
+              value={email}
+              onChange={changeHandler}
+            />
+            {validator.message("email", email, "required|email")}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              className="form-control"
+              type="password"
+              name="password"
+              value={password}
+              onChange={changeHandler}
+            />
+            {validator.message("password", password, "required|between:8,255")}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password_confirmation">Confirm Password</label>
+            <input
+              id="password_confirmation"
+              className="form-control"
+              type="password"
+              name="passwordConfirmation"
+              value={passwordConfirmation}
+              onChange={changeHandler}
+            />
+            {validator.message(
+              "password confirmation",
+              passwordConfirmation,
+              "required|match"
+            )}
+          </div>
+          <input className="btn btn-primary" type="submit" value="Sign Up" />
+        </form>
       </div>
     );
   }
