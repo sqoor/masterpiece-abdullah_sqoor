d58da1d5fb9a35d5bd2d73447f896678652a42f9
sqoor <abdullahalsqoor2@gmail.com>
2020-01-20 11:14:38 +0200

    added cors

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(bodyParser.urlencoded({ extended: false, useNewUrlParser: true }));
 app.use(bodyParser.json());
 app.use("/uploads", express.static("uploads"));
 // app.use(express.static('uploads'))
-// app.use(cors());
+app.use(cors());
 
 app.use((req, res, next) => {
   // req.header('Access-Controll-Allows-Origin', 'http://name-my-page.com')
