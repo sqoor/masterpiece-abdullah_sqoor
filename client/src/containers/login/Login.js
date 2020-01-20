87a325d66b9ae7702be6db7d91dc9336d2a61c0e
sqoor <abdullahalsqoor2@gmail.com>
2020-01-20 08:50:04 +0200

    added validation

--- client/src/containers/login/Login.js
+++ client/src/containers/login/Login.js
@@ -1,78 +1,59 @@
 import React, { Component } from "react";
+
 import Axios from "axios";
+import SimpleReactValidator from "simple-react-validator";
+import { ToastContainer, toast, Zoom } from "react-toastify";
 
 class Login extends Component {
   state = {
-    credentials: {
-      email: "",
-      password: ""
-    },
-    errors: {
-      email: "",
-      password: ""
-    }
+    email: "",
+    password: ""
   };
+
+  constructor(props) {
+    super(props);
+    this.validator = new SimpleReactValidator({
+      element: message => (
+        <div className="alert alert-danger mt-2">{message}</div>
+      )
+    });
+  }
 
   changeHandler = e => {
     const name = e.target.name;
-    const value =  e.target.value;
+    const value = e.target.value;
 
     this.setState(prevState => ({
       ...prevState,
-      credentials: {
-        ...prevState.credentials,
-        [name]: value
-      }
+      [name]: value
     }));
   };
 
-  validate = () => {
-    const { email, password } = this.state.credentials;
-
-    if(email && password) return true;
-
-    if(!email) {
-      this.setState(prevState => ({
-        ...prevState,
-        errors: {
-          ...prevState.errors,
-          message: "Email is required"
-        }
-      }));
-    } else {     
-      this.setState(prevState => ({
-        ...prevState,
-        errors: {
-          ...prevState.errors,
-         message: ""
-        }
-      }));
-    }
-  }
-
-  requestApi = (credentials) => {
-    console.log("requestAPI worked", credentials);
-
-    Axios.post('/users/login', credentials)
-    .then(res => {
-      // do somesthing go to next page if has the token or respond to use unauthorized
-      console.log('RESPONSE', res)
-    })
-    .catch(error => {
-      console.log('ERROR', error);
-    });
-  }
+  requestApi = credentials => {
+    Axios.post("/users/signup", { credentials })
+      .then(res => {
+        // do somesthing go to next page if has the token or respond to use unauthorized
+        console.log("RESPONSE", res);
+      })
+      .catch(error => {
+        console.log("ERROR", error);
+        toast.error("Something went wrong");
+      });
+  };
 
   submitHandler = e => {
     e.preventDefault();
 
-    const{ validate, requestApi, state } = this;
+    const { requestApi, state } = this;
 
-    if(validate()) {
-      requestApi(state.credentials)
+    if (this.validator.allValid()) {
+      requestApi(state);
+      console.log("Logining in...", state);
+    } else {
+      this.validator.showMessages();
+      this.forceUpdate();
+      toast.error("Please fill the empty input fields");
     }
-
-    console.log("Logining in...", state);
   };
 
   render() {
@@ -81,33 +62,42 @@ class Login extends Component {
 
     return (
       <div className="container">
+        <ToastContainer
+          position={toast.POSITION.TOP_LEFT}
+          transition={Zoom}
+          toastClassName="rounded"
+          hideProgressBar={true}
+        />
+
         <h1 className="text-center">Login Page</h1>
-          <form onSubmit={submitHandler}>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>{" "}
-              <input
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>{" "}
+            <input
               className="form-control"
-                id="email"
-                type="email"
-                name="email"
-                value={email}
-                onChange={changeHandler}
-              />
-            </div>
-            
-            <div className="form-group">
-              <label htmlFor="form-control">Password</label>{" "}
-              <input
-                id="password"
-                className="form-control"
-                type="password"
-                name="password"
-                value={password}
-                onChange={changeHandler}
-              />
-            </div>
-            <input className="btn btn-primary" type="submit" value="Login" />
-          </form>
+              id="email"
+              type="email"
+              name="email"
+              value={email}
+              onChange={changeHandler}
+            />
+            {this.validator.message("email", email, "required|email")}
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="form-control">Password</label>{" "}
+            <input
+              id="password"
+              className="form-control"
+              type="password"
+              name="password"
+              value={password}
+              onChange={changeHandler}
+            />
+            {this.validator.message("password", password, "required")}
+          </div>
+          <input className="btn btn-primary" type="submit" value="Login" />
+        </form>
       </div>
     );
   }
