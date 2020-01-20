6cb09c6129bdb31b62dafb1fd3860c26ac844a9e
sqoor <abdullahalsqoor2@gmail.com>
2020-01-20 11:30:07 +0200

    updated navbar 2

--- client/src/containers/navbar/Navbar.js
+++ client/src/containers/navbar/Navbar.js
@@ -4,84 +4,68 @@ import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+      <nav className="navbar navbar-expand-md navbar-dark mb-5 bg-dark">
+        <Link className="navbar-brand" to="/">
+          WASLA
+        </Link>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarsExample08"
-          aria-controls="navbarsExample08"
-          aria-expanded="true"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-
-        <div
-          class="navbar-collapse justify-content-md-center collapse show"
-          id="navbarsExample08"
-          style={{}}
-        >
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Centered nav only <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li class="nav-item">
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav mr-auto">
+            <Link className="nav-link active" to="/">
+              WASLA <span className="sr-only">(current)</span>
+            </Link>
+            <li className="nav-item">
               <Link className="nav-link" to="/learn">
                 Learn
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link className="nav-link" to="">
                 Write
               </Link>
             </li>
-            <li class="nav-item">
+
+            <li className="nav-item">
               <Link className="nav-link" to="">
-                Read/Listen - not implemented
+                Read/Listen
               </Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link className="nav-link" to="/login">
                 Login
               </Link>
             </li>
-            <li class="nav-item">
+
+            <li className="nav-item">
               <Link className="nav-link" to="/signup">
                 Make Account
               </Link>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="http://example.com"
-                id="dropdown08"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="dropdown08">
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
-            </li>
           </ul>
+          <form className="form-inline mt-2 mt-md-0">
+            <input
+              className="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
         </div>
       </nav>
     );
