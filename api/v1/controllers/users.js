3c45fe2e1292889ebe1cfc3d7f582a848fb0807f
sqoor <alsqoorAbdullah@gmail.com>
2020-01-21 10:05:44 +0200

    added validation on users controller, and send token if successed reigstering

--- api/v1/controllers/users.js
+++ api/v1/controllers/users.js
@@ -3,7 +3,18 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
+const validate = user => {
+  if (!user.name || !user.password || !user.email) return false;
+};
+
 exports.singup = (req, res) => {
+  // validate
+  console.log(req.body);
+  if (validate(req.body))
+    return res
+      .json(401)
+      .json({ message: "Not valid request, check input fields" });
+
   User.find({ email: req.body.email }).then(user => {
     if (user.length > 0)
       return res.status(409).json({ message: "User already exists" });
@@ -14,28 +25,43 @@ exports.singup = (req, res) => {
         .json({ message: "Password and confirm password do not match" });
     // return res.status(403).json({ message: "Password and confirm password do not match"})
 
-    bcrypt
-      .hash(req.body.password, 10, (err, hashedPassword) => {
-        if (err) res.status(500).json({ error: err });
-
-        User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: hashedPassword
-        })
-          .then(newUser => {
-            res.status(201).json({
-              message: "Created new user successfully",
-              newUser
-            });
-          })
-          .catch(err => {
-            res.status(500).json({ error: err });
-          });
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      if (err) {
+        console.log("ERROR", err);
+        return res.status(500).json({ error: err });
+      }
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
       })
-      .catch(err => {
-        res.status(500).json({ error: err });
-      });
+        .then(newUser => {
+          const token = jwt.sign(
+            {
+              userId: newUser._id,
+              name: newUser.name,
+              email: newUser.email
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h"
+            }
+          );
+
+          res.status(201).json({
+            message: "Created new user successfully",
+            token,
+            newUser
+          });
+        })
+        .catch(err => {
+          console.log("ERROR", err);
+          res.status(500).json({ error: err });
+        });
+    });
+    // .catch(err => {
+    //   res.status(500).json({ error: err });
+    // });
   });
 };
 
@@ -50,6 +76,7 @@ exports.login = (req, res) => {
           const token = jwt.sign(
             {
               userId: user[0]._id,
+              name: user[0].name,
               email: user[0].email
             },
             process.env.JWT_KEY,
