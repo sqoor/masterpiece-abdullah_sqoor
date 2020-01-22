6e61b9ffaf42bf02d8fd8adc7d1f845d873860f8
sqoor <abdullahalsqoor2@gmail.com>
2020-01-22 13:01:21 +0200

    updated style NotFound page

--- client/src/containers/not-found/NotFound.js
+++ client/src/containers/not-found/NotFound.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
+import "./NotFound.css";
+
+import Helment, { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 class NotFound extends Component {
   render() {
     return (
-      <div>
-        <h1>404 Error</h1>
-        <h3>We can't find the   page</h3>
+      <div className="p-5">
+        <div id="clouds">
+          <div className="cloud x1"></div>
+          <div className="cloud x1_5"></div>
+          <div className="cloud x2"></div>
+          <div className="cloud x3"></div>
+          <div className="cloud x4"></div>
+          <div className="cloud x5"></div>
+        </div>
+        <div className="c">
+          <div className="_404">404</div>
+          <hr />
+          <div className="_1">THE PAGE</div>
+          <div className="_2">WAS NOT FOUND</div>
+          <Link className="btn btn-dark mt-5" to="/">
+            BACK TO MARS
+          </Link>
+        </div>
       </div>
     );
   }
