fc3e42f2b8f65a579e61cf86976504d82f464279
sqoor <abdullahalsqoor2@gmail.com>
2020-01-22 13:05:12 +0200

    fixed a bug: notFound styling overlap other pages

--- client/src/containers/not-found/NotFound.js
+++ client/src/containers/not-found/NotFound.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import "./NotFound.css";
 
-import Helment, { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
 
 class NotFound extends Component {
   render() {
     return (
       <div className="p-5">
+        <Helmet>
+          <link rel="stylesheet" href="css/not-found.css" />
+        </Helmet>
         <div id="clouds">
           <div className="cloud x1"></div>
           <div className="cloud x1_5"></div>
