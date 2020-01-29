a17774c76e4f93bfe2014519977ccf4011b06395
sqoor <abdullahalsqoor2@gmail.com>
2020-01-29 16:12:18 +0200

    implemented the schema of lessons model

--- source_code/api/v1/models/lesson.js
+++ source_code/api/v1/models/lesson.js
@@ -2,8 +2,52 @@ const mongoose = require("mongoose");
 
 const lessonSchema = mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: String, required: true }
+  image: { type: String, required: true },
+  progress: { type: Number },
+  questions: [
+    {
+      _id: {
+        type: mongoose.Types.ObjectId,
+        required: true
+      },
+      language: {
+        question: {
+          type: String,
+          enum: ["ar", "en"],
+          required: true
+        },
+        answer: {
+          type: String,
+          enum: ["ar", "en"],
+          required: true
+        }
+      },
+
+      formate: {
+        type: String,
+        enum: ["text", "audio", "video"],
+        required: true
+      },
+
+      type: {
+        type: String,
+        enum: ["make-sentence", "fill-blank", "multiple-choice"],
+        required: true
+      },
+      question: {
+        type: String,
+        required: true
+      },
+      choices: {
+        type: [String],
+        required: true
+      },
+      answer: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 const Lesson = mongoose.model("Lesson", lessonSchema);
