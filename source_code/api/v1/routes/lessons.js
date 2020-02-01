afa2302b9f0dbf9e12ca312d4f59560601a1a053
sqoor <alsqoorAbdullah@gmail.com>
2020-02-01 09:29:40 +0200

    implemented crud operations on lesson/questions

--- source_code/api/v1/routes/lessons.js
+++ source_code/api/v1/routes/lessons.js
@@ -9,7 +9,7 @@ const Lesson = require("../models/lesson.js");
 // testing routes/
 
 // 1- create lesson
-router.post("/test", async (req, res) => {
+router.post("/", async (req, res) => {
   let createdLesson;
 
   const newLesson = {
@@ -32,7 +32,7 @@ router.post("/test", async (req, res) => {
   }
 });
 
-// get all lessons
+// 2- get all lessons
 router.get("/", async (req, res) => {
   try {
     const lessons = await Lesson.find({});
@@ -43,7 +43,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-// 2- add question
+// 3- add question
 router.post("/:id/question", async (req, res) => {
   try {
     const lessonId = req.params.id;
@@ -60,28 +60,93 @@ router.post("/:id/question", async (req, res) => {
       answer: req.body.answer
     };
 
-    const lesson = await Lesson.updateOne(
+    const result = await Lesson.updateOne(
       { _id: lessonId },
       { $push: { questions: newQuestion } }
     );
 
-    res.status(201).json(lesson);
+    // if(result.n === 1 && result.nModified === 1 && result.ok === 1)
+    res.status(201).json(result);
   } catch (e) {
     console.log("Error adding a question to a lesson", e);
-
     res.status(500).json("Error adding a question to a lesson");
   }
 });
 
+// 4- update lessons
+router.put("/:id", async (req, res) => {
+  try {
+    const lessonId = req.params.id;
+    const updatedLesson = {
+      name: req.body.name,
+      image: req.body.image,
+      progress: req.body.progress
+    };
+
+    const result = await Lesson.updateOne({ _id: lessonId }, updatedLesson);
+    res.status(200).json(result);
+  } catch (e) {
+    console.log("Error updating lesson: ", e);
+    res.status(500).json("Error updating lesson");
+  }
+});
+
+// 5- update question
+router.put("/:id/questions/:questionId", async (req, res) => {
+  const lessonId = req.params.id;
+  const questionId = req.params.questionId;
+
+  const newQuestion = {
+    language: {
+      question: req.body.language.question,
+      answer: req.body.language.answer
+    },
+    formate: req.body.formate,
+    type: req.body.type,
+    question: req.body.question,
+    choices: req.body.choices,
+    answer: req.body.answer
+  };
+
+  const result = await Lesson.updateOne(
+    { _id: lessonId, "questions._id": questionId },
+    { $set: { "questions.$": newQuestion } }
+  );
+
+  return res.status(200).json(result);
+});
+
+// 6- delete lesson
+router.delete("/:id", async (req, res) => {
+  const lessonId = req.params.id;
+  const result = await Lesson.remove({ _id: lessonId });
+
+  return res.status(200).json(result);
+});
+
+// 7- delete question
+router.delete("/:id/questions/:questionId", async (req, res) => {
+  const lessonId = req.params.id;
+  const questionId = req.params.questionId;
+
+  const result = await Lesson.update(
+    { _id: lessonId },
+    { $pull: { questions: { _id: questionId } } }
+  );
+
+  return res.status(200).json(result);
+});
+
 // end testing
 
+/* 
 router.get("/", checkAuth, lessonsController.getAll);
 router.get("/:id", checkAuth, () => lessonsController.getOne);
 router.post("/", checkAuth, lessonsController.create);
 router.put("/:id", checkAuth, () => lessonsController.updateOne);
 router.delete("/:id", checkAuth, () => lessonsController.deleteOne);
 
-/* 
+/*
 router.get("/", checkAuth, ordersController.getAll);
 router.post("/", checkAuth, ordersController.create);
 router.get("/:id", checkAuth, ordersController.getOne);
