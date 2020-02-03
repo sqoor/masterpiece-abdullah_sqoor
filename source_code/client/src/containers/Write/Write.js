36361d9afe42c4c9c1581e01662b423795235177
sqoor <abdullahalsqoor2@gmail.com>
2020-02-03 08:13:21 +0200

    implemented write page

--- source_code/client/src/containers/Write/Write.js
+++ source_code/client/src/containers/Write/Write.js
@@ -8,20 +8,92 @@ export default class Write extends Component {
           Learn to Write In Arabic Lessons
         </h1>
 
-        <div className="container mt-4">
+        <div className="container-fluid mt-5">
           <div className="row d-flex justify-content-between">
-            <div className="col-5 m-2">
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">
+                Part 1 HOW TO WRITE the Arabic Alphabet
+              </h4>
               <iframe
-                width="520"
+                width="670"
+                height="415"
+                src="https://www.youtube.com/embed/S8RO2w3I8AA"
+              ></iframe>
+            </div>
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">
+                Part 2 HOW TO WRITE the Arabic Alphabet
+              </h4>
+              <iframe
+                width="670"
                 height="415"
                 src="https://www.youtube.com/embed/hA3Uji7xmRc"
               ></iframe>
             </div>
-            <div className="col-5 m-2">
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">
+                Part 3 HOW TO WRITE the Arabic Alphabet
+              </h4>
               <iframe
-                width="520"
+                width="670"
                 height="415"
-                src="https://www.youtube.com/embed/tgbNymZ7vqY"
+                src="https://www.youtube.com/embed/YSR6p9TnIv4"
+              ></iframe>
+            </div>
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">
+                Part 4 HOW TO WRITE the Arabic Alphabet
+              </h4>
+              <iframe
+                width="670"
+                height="415"
+                src="https://www.youtube.com/embed/itY0cDzs95o"
+              ></iframe>
+            </div>
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">
+                Part 5 HOW TO WRITE the Arabic Alphabet
+              </h4>
+              <iframe
+                width="670"
+                height="415"
+                src="https://www.youtube.com/embed/p_reEdOrpIc"
+              ></iframe>
+            </div>
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">
+                Part 6 READ & WRITE ARABIC Words with LONG & SHORT Vowels
+              </h4>
+              <iframe
+                width="670"
+                height="415"
+                src="https://www.youtube.com/embed/4m3zC1emOvA"
+              ></iframe>
+            </div>
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">
+                Part 7 ARABIC SOUNDS- LETTER SOUNDS
+              </h4>
+              <iframe
+                width="670"
+                height="415"
+                src="https://www.youtube.com/embed/o1q23Itv_HM"
+              ></iframe>
+            </div>
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">Part 8 HOW TO READ IN ARABIC</h4>
+              <iframe
+                width="670"
+                height="415"
+                src="https://www.youtube.com/embed/zazg7vp3vc4"
+              ></iframe>
+            </div>
+            <div className="col-6 mb-5">
+              <h4 className="text-muted">Part 9 ARABIC POSSESSIVE PRONOUNS</h4>
+              <iframe
+                width="670"
+                height="415"
+                src="https://www.youtube.com/embed/YSR6p9TnIv4"
               ></iframe>
             </div>
           </div>
